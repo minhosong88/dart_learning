052d92b8892f2bd8d4fad3dc5aa2c57280c32777
minhosong88 <hominsong@naver.com>
2024-08-04 14:32:24 -0700

    dart_learning

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  var numbers = {1, 2, 3, 4};
+  Set<String> names = {'Minho', 'Kyle'};
+  print(
+    "$numbers \n",
+  );
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(1);
+
+  print(numbers);
+}
